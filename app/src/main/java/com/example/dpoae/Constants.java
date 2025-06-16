4b374a0e3f6b72d83251d243c700226ff991d7d7
KayaTesla <147280427+KayaTesla@users.noreply.github.com>
2025-06-15 22:15:29 -0700

    tmp

--- app/src/main/java/com/example/dpoae/Constants.java
+++ app/src/main/java/com/example/dpoae/Constants.java
@@ -195,6 +195,14 @@ public class Constants {
             f2[3]=4800;
         }
 
+        else {
+            phone="Unknown";
+            f2[0]=1900;
+            f2[1]=2900;
+            f2[2]=3800;
+            f2[3]=4800;
+        }
+
         freqs=new boolean[f2.length];
         for (int i = 0; i < freqs.length; i++) {
             freqs[i]=true;
